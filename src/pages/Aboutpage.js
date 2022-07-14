1c9404af126ce7a017fd45d16d21b5bc169c3a0d
Mitchell Frey <mfcodingbc@gmail.com>
2022-07-14 17:07:49 -0400

    created pages Created Aboutpage

---
+++ src/pages/Aboutpage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import About from '../components/About';
+
+const Aboutpage = () => {
+  return (
+    <main>
+      <About />
+    </main>
+  );
+};
+
+export default Aboutpage;
